370e720e490665c6edede3e76540b3b29947f43b
Diane Paul <dianepaul@Dianes-MacBook-Air-2.local>
2023-10-23 11:26:04 +0200

    first commit api

---
+++ src/web_service/app_config.py
@@ -0,0 +1,14 @@
+# MODELS
+MODEL_VERSION = "0.0.1"
+PATH_TO_PREPROCESSOR = f"local_models/dv__v{MODEL_VERSION}.pkl"
+PATH_TO_MODEL = f"local_models/model__v{MODEL_VERSION}.pkl"
+
+
+# MISC
+APP_TITLE = "AbaloneAgePredictionApp"
+APP_DESCRIPTION = (
+    "A simple API to predict abalone age in years"
+    "from physical measurements such as lengths"
+    "and various weights."
+)
+APP_VERSION = "0.0.1"
--- src/web_service/main.py
+++ src/web_service/main.py
@@ -1,18 +1,27 @@
-# Code with FastAPI (app = FastAPI(...))
-
-
+from app_config import (
+    APP_DESCRIPTION,
+    APP_TITLE,
+    APP_VERSION,
+    MODEL_VERSION,
+    PATH_TO_MODEL,
+    PATH_TO_PREPROCESSOR,
+)
 from fastapi import FastAPI
 
-# Other imports
+from lib.models import InputData, PredictionOut
+from utils import load_objects, load_preprocessor
 
-app = FastAPI(title="...", description="...")
+app = FastAPI(title=APP_TITLE, description=APP_DESCRIPTION, version=APP_VERSION)
 
 
 @app.get("/")
-def home() -> dict:
+def home():
     return {"health_check": "App up and running!"}
 
 
-@app.post("/predict", response_model="InsertHereAPydanticClass", status_code=201)
-def predict(payload: "InsertHereAPydanticClass") -> dict:
-    # TODO: complete and replace the "InsertHereAPydanticClass" with the correct Pydantic classes defined in web_service/lib/models.py
+@app.post("/predict", response_model=PredictionOut, status_code=201)
+def predict(payload: InputData):
+    dv = load_preprocessor(PATH_TO_PREPROCESSOR)
+    model = load_model(PATH_TO_MODEL)
+    y = run_inference([payload], dv, model)
+    return {"abalone_age_prediction": y[0]}

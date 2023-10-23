9f25416bba08e5b631ad8d24f3fa586519d54d24
Diane Paul <dianepaul@Dianes-MacBook-Air-2.local>
2023-10-23 11:36:24 +0200

    add cat var

--- src/web_service/app_config.py
+++ src/web_service/app_config.py
@@ -2,6 +2,7 @@
 MODEL_VERSION = "0.0.1"
 PATH_TO_PREPROCESSOR = f"local_models/dv__v{MODEL_VERSION}.pkl"
 PATH_TO_MODEL = f"local_models/model__v{MODEL_VERSION}.pkl"
+CATEGORICAL_VARS = ["Sexe"]
 
 
 # MISC

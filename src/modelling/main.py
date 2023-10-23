50a1e4e7d10b5cedf2b293dadb9389631a57cc15
ConstanceGlly <constanceguelluy@gmail.com>
2023-10-23 13:48:43 +0200

    main and preproccessing

--- src/modelling/main.py
+++ src/modelling/main.py
@@ -1,20 +1,39 @@
 # This module is the training flow: it reads the data, preprocesses it, trains a model and saves it.
 
 import argparse
+import pandas as pd 
+import numpy as np 
+from preprocessing import compute_target, encode_categorical_cols, scale, extract_x_y
+from utils import load_pickle, save_pickle
+from training import train_model
+import os 
 
-
-def main(trainset_path: Path) -> None:
+def main(trainset_path: str, select_train=True) -> None:
     """Train a model using the data at the given path and save the model (pickle)."""
     # Read data
+    df = pd.read_csv(trainset_path)
 
     # Preprocess data
+    df = compute_target(df)
+    df = encode_categorical_cols(df)
+    df = scale(df)
+    X_train, X_test, y_train, y_test = extract_x_y(df)
+    
+    # Directory paths 
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    # Get the directory containing the current script (your current file)
+    model_save_path = os.path.join(os.path.dirname(os.path.dirname(script_dir), "web_dev", "loaded_model.pkl"))  # Save to the "web_dev" directory
+    
+    # Train the model and save it 
+    if select_train:
+        # Train model
+        model = train_model(X_train, y_train)
+        save_pickle(model_save_path, model)
 
-    # (Optional) Pickle encoder if need be
-
-    # Train model
-
-    # Pickle model --> The model should be saved in pkl format the `src/web_service/local_objects` folder
-
+    else : 
+        model_load_path = os.path.join(script_dir, "training")
+        loaded_model = load_pickle(model_load_path)
+        save_pickle(model_save_path, loaded_model)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train a model using the data at the given path.")
--- src/modelling/preprocessing.py
+++ src/modelling/preprocessing.py
@@ -21,15 +21,13 @@ def encode_categorical_cols(df: pd.DataFrame, categorical_cols: List[str] = None
     df = pd.get_dummies(df, columns=categorical_cols, drop_first=True)
     return df
 
+def scale(X: pd.DataFrame) -> pd.DataFrame:
+    # StandardScaler
+    X_scaled = StandardScaler().fit_transform(X)
+    return X_scaled
 
 def extract_x_y(df: pd.DataFrame) -> dict:
     # Extract X and y
     X, y = df.drop(DROP_COLS, axis=1), df["Age"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
-
-
-def scale(X: pd.DataFrame) -> pd.DataFrame:
-    # StandardScaler
-    X_scaled = StandardScaler().fit_transform(X)
-    return X_scaled
